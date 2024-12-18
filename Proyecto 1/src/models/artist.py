060ae7c9c08ca94a83935a2127cd4c155ff47b01
ricardious <castaneda.systems@gmail.com>
2024-12-17 23:29:39 -0600

    feat(models): add Artist class for managing artist information

---
+++ Proyecto 1/src/models/artist.py
@@ -0,0 +1,11 @@
+class Artist:
+    def __init__(
+        self, artist_id, pwd, full_name, email, phone, specialties, additional_notes
+    ):
+        self.artist_id = artist_id
+        self.pwd = pwd
+        self.full_name = full_name
+        self.email = email
+        self.phone = phone
+        self.specialties = specialties
+        self.additional_notes = additional_notes
