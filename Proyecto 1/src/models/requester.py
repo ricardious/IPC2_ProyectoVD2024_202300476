5183052fadaefc46960ed9603ed104d37bcd1f8c
ricardious <castaneda.systems@gmail.com>
2024-12-17 23:30:23 -0600

    feat(models): add Requester class for managing requester information

---
+++ Proyecto 1/src/models/requester.py
@@ -0,0 +1,8 @@
+class Requester:
+    def __init__(self, requester_id, pwd, full_name, email, phone, address):
+        self.requester_id = requester_id
+        self.pwd = pwd
+        self.full_name = full_name
+        self.email = email
+        self.phone = phone
+        self.address = address
