c0599900ba0850fec6799099ea95191f4037d5f4
ricardious <castaneda.systems@gmail.com>
2024-12-26 17:38:50 -0600

    feat(global_state): initialize global data structures for application state management

--- Proyecto 1/src/global_state.py
+++ Proyecto 1/src/global_state.py
@@ -1,5 +1,7 @@
 from models.linked_list import LinkedList
 from models.doubly_linked_list import DoublyLinkedList
+from models.queue import Queue
 
 artist_list = LinkedList()
 requester_list = DoublyLinkedList()
+request_queue = Queue()
